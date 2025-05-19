77beb13f17857b0817faf6cdad10448f4b835283
Thủy Nguyễn Văn <thuy956806@gmail.com>
2025-05-19 04:04:03 +0000

    no, i change my mind, roll back

--- src/components/day-weaver/TaskForm.tsx
+++ src/components/day-weaver/TaskForm.tsx
@@ -113,7 +113,6 @@ export const TaskForm: FC<TaskFormProps> = ({ onAddTask }) => {
                         selected={field.value}
                         onSelect={field.onChange}
                         initialFocus
-                        weekStartsOn={1} // Set week to start on Monday
                         disabled={(date) => date < new Date(new Date().setDate(new Date().getDate() -1))}
                       />
                     </PopoverContent>
